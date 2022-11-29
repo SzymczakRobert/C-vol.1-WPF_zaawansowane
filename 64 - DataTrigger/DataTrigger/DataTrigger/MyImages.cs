a1480fcf2ad72c6c7dc49d044bb81b2b34c52feb
SzymczakRobert <97690534+SzymczakRobert@users.noreply.github.com>
2022-11-29 20:44:06 +0100

    Add files via upload

---
+++ 64 - DataTrigger/DataTrigger/DataTrigger/MyImages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTrigger
+{
+    class MyImages
+    {
+        public string pathToImage { get; set; }
+        public string nameOfImage { get; set; }
+        public string isFound { get; set; }
+
+        public MyImages()
+        {
+            this.pathToImage = "zabka.jpg";
+            this.nameOfImage = "Logo";
+
+        }
+    }
+}
