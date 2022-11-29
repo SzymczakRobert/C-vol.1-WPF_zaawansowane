2ddb81d7ca8630f5a00edbb1927826be88360246
SzymczakRobert <97690534+SzymczakRobert@users.noreply.github.com>
2022-11-29 20:43:08 +0100

    Add files via upload

---
+++ 60 - Converter/Converter/Converter/BoolVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converter
+{
+    //tu warto coś napisać o kolejności konwertowania
+    // czyli z czego konwertujemu => do czego
+
+    [ValueConversion(typeof(bool),typeof(Visibility))]
+    class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+            if (isVisible == true) // dla pewności warto dopisać przypisanie "==true"
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // pierwsza konwersja poniżej nie jest potrzebna!!
+
+            //  throw new NotImplementedException();
+
+            //jeśli była  by potrzebna konwersja w jedna i druga stronę to klepiemy to poniżej
+            Visibility visibility = (Visibility)value;
+
+            if (visibility == Visibility.Collapsed)
+                return false;
+            else
+                return true;
+
+            // można zwrócić null co by się nc nie działo
+
+            // return null;
+        }
+    }
+}
